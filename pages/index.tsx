ee68b6a49b4acab08be8b8e1fa0dbda7ac0a8b6e
iam-dev <elky.bachtiar@duo.nl>
2022-06-27 09:42:44 +0200

    footer link color

--- pages/index.tsx
+++ pages/index.tsx
@@ -249,17 +249,17 @@ const Home: NextPage = () => {
                 </div>
             </main>
             <footer className={styles.footer}>
-                <a
-                href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                target="_blank"
-                rel="noopener noreferrer"
-                >
-                Powered by{' '}
-                <span className={styles.logo}>
-                    <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-                </span>
-                </a>
-                <Link href="https://github.com/zkWallet/zkWallet-docs">Github documentation</Link>
+                <Link color="#FFFFFF"
+                    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    >
+                    Powered by{' '}
+                    <span className={styles.logo}>
+                        <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+                    </span>
+                </Link>
+                <Link color="#FFFFFF" href="https://github.com/zkWallet/zkWallet-docs">Github documentation</Link>
             </footer>
         </div>
     )
